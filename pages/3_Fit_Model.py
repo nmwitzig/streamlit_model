c7ce15e6aae7fb50ff0b1ac3554838761b78203d
Niklas Witzig <niklas.witzig@uni-mainz.de>
2023-12-12 17:48:11 +0100

    some changes

--- pages/3_Fit_Model.py
+++ pages/3_Fit_Model.py
@@ -11,7 +11,7 @@ from numpyro.infer import MCMC, NUTS
 import jax
 import streamlit as st
 import arviz as az
-numpyro.set_host_device_count(1)
+numpyro.set_host_device_count(4)
 
 st.title('Fit Model (MCMC)')
 st.sidebar.markdown("# Fit Model (MCMC)")
@@ -29,7 +29,7 @@ rng_key = jax.random.PRNGKey(0)
 @st.cache_data
 def run_mcmc():
     nuts_kernel = NUTS(model_main)
-    mcmc = MCMC(nuts_kernel, num_warmup=SAMPLES, num_samples=SAMPLES, num_chains=1, progress_bar=True, chain_method='parallel')
+    mcmc = MCMC(nuts_kernel, num_warmup=SAMPLES, num_samples=SAMPLES, num_chains=4, progress_bar=True, chain_method='parallel')
     mcmc.run(rng_key,x1=x1,x2=x2, y=y, condition_id=condition_id, subject_id=subject_id)
     az_mcmc = az.from_numpyro(mcmc)
     return az_mcmc
@@ -45,7 +45,7 @@ az_summary = get_summary()
 st.dataframe(az_summary)
 
 
-st.markdown("### Traceplot")
-az_mcmc = run_mcmc()
-trace_plot = az.plot_trace(az_mcmc, var_names=['nu_mu'], compact=True, divergences=None)
-st.pyplot(trace_plot)
+# st.markdown("### Traceplot")
+# az_mcmc = run_mcmc()
+# trace_plot = az.plot_trace(az_mcmc, var_names=['nu_mu'], compact=True, divergences=None)
+# st.pyplot(trace_plot)
